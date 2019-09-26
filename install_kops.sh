db0f17c68c374715f2692f1125ebc8abde9710d2
Demian <hagen_id@ukr.net>
2019-09-26 15:02:05 +0300

    update
    Create Cluster for domain localdevops.com

--- install_kops.sh
+++ install_kops.sh
@@ -1,4 +1,18 @@
 #!/bin/bash 
 
+export REGION=us-east-1
+export KOPS_STATE_STORE=s3://kops.localdevops.com
+export NAME=localdevops.com
+
 sudo -s apt-get -y update && apt-get -y upgrade
+
+
+
+# Create Cluster for domain localdevops.com
+
+kops create cluster --zones us-east-1b \
+		    --master-size t2.micro \ 
+		    --master-count 1 \
+		    --node-size t2.micro \
+		    --node-count 3 ${NAME}
 
